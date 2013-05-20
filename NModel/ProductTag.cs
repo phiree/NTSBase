e93b8e68e0a4802458bd7fe9ca671d846375570f
phiree <phiree@gmail.com>
2013-05-20 18:03:48 +0800

    产品集 相关文件

---
+++ NModel/ProductTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NModel
+{
+    //产品标签-
+   public class ProductTag
+    {
+       public virtual Guid Id { get; set; }
+       public virtual string TagName { get; set; }
+       public virtual string Description { get; set; }
+       public virtual DateTime CreateTime { get; set; }
+    }
+}
---
+++ Web/Admin/Products/CreateProductTag.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NBiz;
+using NModel;
+public partial class Admin_Products_CreateProductTag : System.Web.UI.Page
+{
+    BizProduct bizProduct = new BizProduct();
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BuildProductList()
+    {
+        IList<NModel.Product> productList = bizProduct.GetListByProvidedModelNumberSupplierNameList(tbxProductList.Text);
+
+
+       
+        
+    }
+    
+
+
+}
